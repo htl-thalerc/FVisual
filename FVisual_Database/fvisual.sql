3cca8278ec63d73e3a05d16d44869fc294c2f3e9
thalerc <thalerc@edu.htl-villach.at>
2019-10-03 18:58:28 +0200

    updated database sql

--- FVisual_Database/fvisual.sql
+++ FVisual_Database/fvisual.sql
@@ -4,11 +4,11 @@ DROP TABLE Einsatzfahrzeuge CASCADE CONSTRAINTS;
 DROP TABLE Stuetzpunktorte CASCADE CONSTRAINTS;
 DROP TABLE Einsatzorte CASCADE CONSTRAINTS;
 DROP TABLE Stuetzpunkte CASCADE CONSTRAINTS;
-DROP TABLE Fahrzeug_gehoert_Stuetzpunkt CASCADE CONSTRAINTS;
-DROP TABLE Mitglied_gehoert_Stuetzpunkt CASCADE CONSTRAINTS;
 DROP TABLE Einsatzarten CASCADE CONSTRAINTS;
 DROP TABLE Einsatzcodes CASCADE CONSTRAINTS;
+DROP TABLE ANDERE_ORGANISATIONEN CASCADE CONSTRAINTS;
 DROP TABLE Einsaetze CASCADE CONSTRAINTS;
+DROP TABLE AOrg_war_bei_Einsatz CASCADE CONSTRAINTS;
 DROP TABLE Einsatzkraft_war_bei_Einsatz CASCADE CONSTRAINTS;
 DROP TABLE Mitglied_war_bei_Einsatz CASCADE CONSTRAINTS;
 DROP TABLE Fahrzeug_war_bei_Einsatz CASCADE CONSTRAINTS;
@@ -26,8 +26,10 @@ CREATE TABLE Mitglieder
   id            INTEGER,
   vorname       VARCHAR(30),
   nachname      VARCHAR(30),
+  id_stuetzpunkt INTEGER,
   id_dienstgrad INTEGER,
   CONSTRAINT pk_Mitglieder PRIMARY KEY (id),
+  CONSTRAINT fk_Einsatzfahrzeug_Stuetzpunkt FOREIGN KEY (id_stuetzpunkt) REFERENCES Stuetzpunkte (id),
   CONSTRAINT fk_Dienstgrad FOREIGN KEY (id_dienstgrad) REFERENCES Dienstgrade (id)
 );
 
@@ -35,7 +37,9 @@ CREATE TABLE Einsatzfahrzeuge
 (
   id      INTEGER,
   rufname VARCHAR(30),
+  id_stuetzpunkt INTEGER,
   CONSTRAINT pk_Einsatzfahrzeuge PRIMARY KEY (id),
+  CONSTRAINT fk_Einsatzfahrzeug_Stuetzpunkt FOREIGN KEY (id_stuetzpunkt) REFERENCES Stuetzpunkte (id),
   CONSTRAINT uq_Einsatzfahrzeug UNIQUE (rufname)
 );
 
@@ -61,28 +65,10 @@ CREATE TABLE Stuetzpunkte
 (
   id   INTEGER,
   name VARCHAR(50),
-  CONSTRAINT pk_STPNKT PRIMARY KEY (id)
+  CONSTRAINT pk_STPNKT PRIMARY KEY (id),
+  CONSTRAINT uq_Stuetzpunkte UNIQUE (name)
 );
 
-CREATE TABLE Fahrzeug_gehoert_Stuetzpunkt
-(
-  id_stuetzpunkt     INTEGER,
-  id_einsatzfahrzeug INTEGER,
-  CONSTRAINT pk_FZG_gehoert_STPNKT PRIMARY KEY (id_stuetzpunkt, id_einsatzfahrzeug),
-  CONSTRAINT fk_FZG_in_STPNKT FOREIGN KEY (id_stuetzpunkt) REFERENCES Stuetzpunkte (id),
-  CONSTRAINT fk_FZG_Reference FOREIGN KEY (id_einsatzfahrzeug) REFERENCES Einsatzfahrzeuge (id),
-  CONSTRAINT uq_FZG UNIQUE (id_einsatzfahrzeug)
-);
-
-CREATE TABLE Mitglied_gehoert_Stuetzpunkt
-(
-  id_stuetzpunkt INTEGER,
-  id_mitglied    INTEGER,
-  CONSTRAINT pk_M_gehoert_zu_STPNKT PRIMARY KEY (id_stuetzpunkt, id_mitglied),
-  CONSTRAINT fk_STPNKT_von_M FOREIGN KEY (id_stuetzpunkt) REFERENCES Stuetzpunkte (id),
-  CONSTRAINT fk_Mitglied_Reference FOREIGN KEY (id_mitglied) REFERENCES Mitglieder (id),
-  CONSTRAINT uq_Mitglied UNIQUE (id_mitglied)
-);
 
 CREATE TABLE Einsatzarten
 (
@@ -97,6 +83,13 @@ CREATE TABLE Einsatzcodes
   code VARCHAR(30),
   CONSTRAINT pk_Einsatzcodes PRIMARY KEY (id)
 
+);
+
+CREATE TABLE ANDERE_ORGANISATIONEN(
+  id           INTEGER,
+  name VARCHAR(100),
+  CONSTRAINT pk_Einsatzarten PRIMARY KEY (id),
+  CONSTRAINT uq_ANDERE_ORGANISATIONEN UNIQUE(name)
 );
 
 CREATE TABLE Einsaetze
@@ -115,6 +108,14 @@ CREATE TABLE Einsaetze
   CONSTRAINT fk_Einsaetzte_Einsatzcode FOREIGN KEY (id_einsatzcode) REFERENCES Einsatzcodes (id)
 );
 
+CREATE TABLE AOrg_war_bei_Einsatz(
+    id_einsatz INTEGER,
+    id_andere_org INTEGER,
+    CONSTRAINT fk_Einsatz FOREIGN KEY (id_Einsatz) REFERENCES Einsaetze (id),
+     CONSTRAINT fk_ANDERE_ORGANISATIONEN FOREIGN KEY (id_andere_org) REFERENCES ANDERE_ORGANISATIONEN (id)
+
+);
+
 CREATE TABLE Einsatzkraft_war_bei_Einsatz
 (
   id_Stuetzpunkt INTEGER,
@@ -130,7 +131,7 @@ CREATE TABLE Mitglied_war_bei_Einsatz
   id_Stuetzpunkt INTEGER,
   id_Einsatz     INTEGER,
   CONSTRAINT pk_Mitglied_war_bei_Einsatz PRIMARY KEY (id_Mitglied, id_Stuetzpunkt, id_einsatz),
-  CONSTRAINT fk_Mitglied_bei_Mitglied FOREIGN KEY (id_Mitglied, id_Stuetzpunkt) REFERENCES Mitglied_gehoert_Stuetzpunkt (id_mitglied, id_Stuetzpunkt),
+  CONSTRAINT fk_Mitglied_war_bei_Einsatz FOREIGN KEY (id_Mitglied, id_Stuetzpunkt) REFERENCES Mitglieder (id_mitglied),
   CONSTRAINT fk_Einsatzkraft_bei_Einsatz FOREIGN KEY (id_Stuetzpunkt, id_Einsatz) REFERENCES Einsatzkraft_war_bei_Einsatz (id_Stuetzpunkt, id_Einsatz)
 );
 
@@ -140,6 +141,6 @@ CREATE TABLE Fahrzeug_war_bei_Einsatz
   id_Stuetzpunkt     INTEGER,
   id_Einsatz         INTEGER,
   CONSTRAINT pk_Fahrzeug_war_bei_Einsatz PRIMARY KEY (id_Einsatzfahrzeug, id_Stuetzpunkt, id_einsatz),
-  CONSTRAINT fk_FZG_war_bei_Einsatz FOREIGN KEY (id_Einsatzfahrzeug, id_Stuetzpunkt) REFERENCES Fahrzeug_gehoert_Stuetzpunkt (id_einsatzfahrzeug, id_stuetzpunkt),
+  CONSTRAINT fk_FZG_war_bei_Einsatz FOREIGN KEY (id_Einsatzfahrzeug, id_Stuetzpunkt) REFERENCES Einsatzfahrzeuge (id_einsatzfahrzeug),
   CONSTRAINT fk_EKR_war_bei_Einsatz FOREIGN KEY (id_Stuetzpunkt, id_Einsatz) REFERENCES Einsatzkraft_war_bei_Einsatz (id_Stuetzpunkt, id_Einsatz)
 );
