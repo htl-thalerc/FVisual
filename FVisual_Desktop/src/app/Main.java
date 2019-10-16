bfe9b8a00cbd216fe7c94eae1d0b667d10bbdedf
Sandro19 <asseks@edu.htl-villach.at>
2019-10-16 12:11:36 +0200

    Loading different anchorpanes for each management

--- FVisual_Desktop/src/app/Main.java
+++ FVisual_Desktop/src/app/Main.java
@@ -12,9 +12,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("/gui/Mainframe.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,1200,800);
 			scene.getStylesheets().add(getClass().getResource("/gui/application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("FVisual Management Tool");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- FVisual_Desktop/src/controller/ControllerMainframe.java
+++ FVisual_Desktop/src/controller/ControllerMainframe.java
@@ -1,11 +1,74 @@
 package controller;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.MenuItem;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class ControllerMainframe {
-	@FXML private MenuItem mitemOperationManagement;
-	@FXML private MenuItem mitemBaseManagement;
-	@FXML private MenuItem mitemProfileSetting;
-	@FXML private MenuItem mitemLogout;
+import com.sun.prism.paint.Color;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+public class ControllerMainframe implements Initializable {
+	@FXML
+	private MenuItem mItemOperationManagement;
+	@FXML
+	private MenuItem mItemBaseManagement;
+	@FXML
+	private MenuItem mItemProfileSettings;
+	@FXML
+	private MenuItem mItemLogout;
+	@FXML
+	private AnchorPane mainPane;
+	private ArrayList<Node> middlePaneContent = new ArrayList<>();
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+	}
+
+	@FXML
+	private void onClickmItemOperationManagement(ActionEvent aE) {
+		AnchorPane operationManagementPane = null;
+		try {
+			this.middlePaneContent.clear();
+			this.middlePaneContent.addAll(this.mainPane.getChildren());
+			this.mainPane.getChildren().clear();
+			operationManagementPane = FXMLLoader.load(getClass().getResource("/gui/OperationManagement.fxml"));
+			this.mainPane.getChildren().add(operationManagementPane);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	private void onClickmItemBaseManagement(ActionEvent aE) {
+		AnchorPane baseManagementPane = null;
+		try {
+			this.middlePaneContent.clear();
+			this.middlePaneContent.addAll(this.mainPane.getChildren());
+			this.mainPane.getChildren().clear();
+			baseManagementPane = FXMLLoader.load(getClass().getResource("/gui/BaseManagement.fxml"));
+			this.mainPane.getChildren().add(baseManagementPane);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	private void onClickmItemProfileSettings(ActionEvent aE) {
+
+	}
+
+	@FXML
+	private void onClickmItemLogout(ActionEvent aE) {
+
+	}
 }
