e0c70ea7b9cc1b9586c8d22de61ee1b1e4e97b36
htl-thalerc <thalerchristoph@gmx.at>
2019-10-30 12:57:41 +0100

    installation and setup log4js

--- FVisual_Webservice/modules/response-handler.js
+++ FVisual_Webservice/modules/response-handler.js
@@ -1,20 +1,25 @@
 'use strict'
 
+/* node modules */
+const log4js = require('log4js');
+
 /* own modules */
 const oracleJobs = require('../database/oracle-jobs');
+
+/* local variables */
+const logger = log4js.getLogger("Response-Handler");
+logger.level = 'warn';
 
 var res_GET_DEFAULT = function (res, parser) {
   return function (err, result) {
     if (err) {
-      console.warn(err)
-      res.status(500).send({
-        code: err.errorNum,
-        message: err.message,
-        offset: err.offset
-      });
+      logger.error(err.stack);
+      res.status(500).send(err.stack);
+      logger.debug("execute error sent!");
     } else {
       res.header('content-type', 'application/json');
       res.status(200).send(parser(JSON.stringify(result.rows)));
+      logger.debug("response sent!");
     }
   };
 };
@@ -22,24 +27,19 @@ var res_GET_DEFAULT = function (res, parser) {
 var res_POST_DEFAULT = function (res, querystring, params, parser) {
   return function (err, result) {
     if (err) {
-      console.warn(err);
-      res.status(500).send({
-        code: err.errorNum,
-        message: err.message,
-        offset: err.offset
-      });
+      logger.error(err.stack);
+      res.status(500).send(err.stack);
+      logger.debug("execute error sent!");
     } else {
       oracleJobs.execute(querystring, params, (err, result) => {
         if (err) {
-          console.warn(err);
-          res.status(500).send({
-            code: err.errorNum,
-            message: err.message,
-            offset: err.offset
-          });
+          logger.error(err.stack);
+          res.status(500).send(err.stack);
+          logger.debug("nested execute error sent!");
         } else {
           res.header('content-type', 'application/json');
           res.status(201).send(parser(JSON.stringify(result.rows)));
+          logger.debug("response sent!");
         }
       });
     }
@@ -49,24 +49,19 @@ var res_POST_DEFAULT = function (res, querystring, params, parser) {
 var res_PUT_DEFAULT = function (res, querystring, params, parser) {
   return function (err, result) {
     if (err) {
-      console.warn(err);
-      res.status(500).send({
-        code: err.errorNum,
-        message: err.message,
-        offset: err.offset
-      });
+      logger.error(err.stack);
+      res.status(500).send(err.stack);
+      logger.debug("execute error sent!");
     } else {
       oracleJobs.execute(querystring, params, (err, result) => {
         if (err) {
-          console.warn(err);
-          res.status(500).send({
-            code: err.errorNum,
-            message: err.message,
-            offset: err.offset
-          });
+          logger.error(err.stack);
+          res.status(500).send(err.stack);
+          logger.debug("nested execute error sent!");
         } else {
           res.header('content-type', 'application/json');
           res.status(200).send(parser(JSON.stringify(result.rows)));
+          logger.debug("response sent!");
         }
       })
     }
@@ -76,14 +71,12 @@ var res_PUT_DEFAULT = function (res, querystring, params, parser) {
 var res_DELETE_DEFAULT = function (res) {
   return function (err, result) {
     if (err) {
-      console.warn(err);
-      res.status(500).send({
-        code: err.errorNum,
-        message: err.message,
-        offset: err.offset
-      });
+      logger.error(err.stack);
+      res.status(500).send(err.stack);
+      logger.debug("execute error sent!");
     } else {
       res.status(204).send(result);
+      logger.debug("response sent!");
     }
   };
 }
--- FVisual_Webservice/public/central_error_handler.js
+++ FVisual_Webservice/public/central_error_handler.js
@@ -1,7 +1,15 @@
 'use strict';
 
+/* node modules */
+const log4js = require('log4js');
+
+/* local variables */
+const logger = log4js.getLogger("Central-Error-Handler");
+logger.level = 'error';
+
 module.exports.errorHandler = function (err, req, res, next) {
-    console.log(err.stack);
+    logger.fatal(err);
+    logger.error(err.stack);
     if (err instanceof TypeError)
         res.status(400).send('400 Bad Request. ' + err.message);
     else if (err instanceof RangeError)
