441fda657eea5474f8832b0c808c273456a5e374
htlvil-drabosea <39909402+htlvil-drabosea@users.noreply.github.com>
2019-11-19 20:36:54 +0100

    Icons geaddet, Added some Marker for map

--- MyApplication/app/src/main/java/com/example/myapplication/MapsActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -1,12 +1,21 @@
 package com.example.myapplication;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.DrawableRes;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.common.util.MapUtils;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,7 +29,6 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +39,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, @DrawableRes int vectorDrawableResourceId) {
+        Drawable background = ContextCompat.getDrawable(context, R.drawable.ic_placeholder);
+        background.setBounds(0, 0, background.getIntrinsicWidth(), background.getIntrinsicHeight());
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorDrawableResourceId);
+        vectorDrawable.setBounds(23, 17, vectorDrawable.getIntrinsicWidth() + 23, vectorDrawable.getIntrinsicHeight() + 17);
+        Bitmap bitmap = Bitmap.createBitmap(background.getIntrinsicWidth(), background.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        background.draw(canvas);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(46.7525, 14))
+                .icon(bitmapDescriptorFromVector(this, R.drawable.ic_home_black_24dp))
+                .title("Stützpunkt")
+        );
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(46.7525, 14.88))
+                .icon(bitmapDescriptorFromVector(this, R.drawable.ic_fire))
+                .title("Feuer")
+        );
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(46.9525, 14.88))
+                .icon(bitmapDescriptorFromVector(this, R.drawable.ic_sports_car))
+                .title("Unfall")
+        );
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(46.3525, 14.7))
+                .icon(bitmapDescriptorFromVector(this, R.drawable.ic_tools_cross_settings_symbol_for_interface))
+                .title("Technischer Einsatz")
+        );
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(46.4525, 14.88))
+                .icon(bitmapDescriptorFromVector(this, R.drawable.ic_customer_service))
+                .title("Hilfeleistung")
+        );
+
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(46.3525, 14.78))
+                .icon(bitmapDescriptorFromVector(this, R.drawable.ic_man_user))
+                .title("Personensuche")
+        );
+
+
+        /*
         Drawable circleDrawable = getResources().getDrawable(R.drawable.einsaetze_circle);
-        BitmapDescriptor markerIcon = getMarkerIconFromDrawable(circleDrawable);
+        Bi
 
         mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(46.7525, 13.8617))
-                    .title("Einsatz")
-            .icon(markerIcon));
+                .snippet("E")
+                .position()
+                .title("Einsatz")
+                .icon(markerIcon));
 
 
         circleDrawable = getResources().getDrawable(R.drawable.stuetzpunkte_circle);
@@ -49,19 +107,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .position(new LatLng(46.7525, 14))
                 .title("Stuetzpunkt")
                 .icon(markerIcon));
-
+*/
         CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(46.7525, 13.8617)).zoom(7.5f).build();
         CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
-        mMap.moveCamera(cameraUpdate);
-    }
-
-    private BitmapDescriptor getMarkerIconFromDrawable(Drawable drawable) {
-        Canvas canvas = new Canvas();
-        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        canvas.setBitmap(bitmap);
-        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
-        drawable.draw(canvas);
-        return BitmapDescriptorFactory.fromBitmap(bitmap);
-
+        googleMap.moveCamera(cameraUpdate);
     }
 }
