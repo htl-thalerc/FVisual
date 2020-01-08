52a09b2faa1892cc4134d9c7056dbaeda03b2ca8
htlvil-drabosea <39909402+htlvil-drabosea@users.noreply.github.com>
2020-01-08 08:27:04 +0100

    little changes

--- MyApplication/app/src/main/java/com/example/myapplication/bll/Einsatz.java
+++ MyApplication/app/src/main/java/com/example/myapplication/bll/Einsatz.java
@@ -1,20 +1,21 @@
 package com.example.myapplication.bll;
 
-
-import com.example.myapplication.bll.Einsatzart;
+import java.util.Date;
+import java.util.Timer;
 
 public class Einsatz {
     private int id ;
     private Einsatzart einsatzart ;
-    private String einsatzcode ;
+    private Einsatzcode einsatzcode ;
     private String einsatzort ;
     private String titel;
     private String kurzbeschreibung;
-    private String datum;
-    private String zeit;
-    private String feuerwehren;
+    private Date datum;
+    private Timer zeit;
+    private int mannstaerke;
+    //private String feuerwehren;
 
-    public Einsatz(int id, Einsatzart einsatzart, String einsatzcode, String einsatzort, String titel, String kurzbeschreibung, String datum, String zeit, String feuerwehren) {
+    public Einsatz(int id, Einsatzart einsatzart, Einsatzcode einsatzcode, String einsatzort, String titel, String kurzbeschreibung, Date datum, Timer zeit, String feuerwehren) {
         this.id = id;
         this.einsatzart = einsatzart;
         this.einsatzcode = einsatzcode;
@@ -23,7 +24,7 @@ public class Einsatz {
         this.kurzbeschreibung = kurzbeschreibung;
         this.datum = datum;
         this.zeit = zeit;
-        this.feuerwehren = feuerwehren;
+        //this.feuerwehren = feuerwehren;
     }
 
     public Einsatz() {
---
+++ MyApplication/app/src/main/java/com/example/myapplication/bll/Einsatzcode.java
@@ -0,0 +1,5 @@
+package com.example.myapplication.bll;
+
+enum Einsatzcode {
+    P_100
+}
