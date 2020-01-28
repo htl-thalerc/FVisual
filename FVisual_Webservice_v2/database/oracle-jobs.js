7a347ca6fef772b06c1a5269d8237ad786ea5123
Christoph Thaler <thalerc@edu.htl-villach.at>
2020-01-28 11:53:02 +0100

    Update oracle-jobs.js

--- FVisual_Webservice_v2/database/oracle-jobs.js
+++ FVisual_Webservice_v2/database/oracle-jobs.js
@@ -11,7 +11,7 @@ const usn = `d5a20`;
 const pwd = `d5a`;
 
 //intern
-const cStr = `10.0.6.111:1521/ora11g`;
+const cStr = `192.168.193.243:1521/ora11g`;
 
 //extern
 //const cStr = `212.152.179.117:1521/ora11g`;
